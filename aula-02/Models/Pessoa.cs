d2a74825ee3ea027166e270fd48f3015d7be7000
Henrique Rosa <rosa.henrique@outlook.com.br>
2022-10-27 14:07:06 -0300

    aula-02

---
+++ aula-02/Models/Pessoa.cs
@@ -0,0 +1,9 @@
+namespace aula_02.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string CPF { get; set; }
+    }
+}
