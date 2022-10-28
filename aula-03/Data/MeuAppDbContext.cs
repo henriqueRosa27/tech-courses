daaec529bb9bd0f934119241580c69ea50414b3a
Henrique Rosa <rosa.henrique@outlook.com.br>
2022-10-28 17:56:26 -0300

    ajuste final para aula

--- aula-03/Data/MeuAppDbContext.cs
+++ aula-03/Data/MeuAppDbContext.cs
@@ -6,16 +6,11 @@ namespace aula_03.Data
     public class MeuAppDbContext : DbContext
     {
 
-        public string DbPath { get; }
-
         public MeuAppDbContext()
         {
-            var folder = Environment.SpecialFolder.LocalApplicationData;
-            var path = Environment.GetFolderPath(folder);
-            DbPath = System.IO.Path.Join(path, "meu-app.db");
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite($"Data Source={DbPath}");
+            => options.UseSqlite("Data Source=meu-app.db");
         }
 }
